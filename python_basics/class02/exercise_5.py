5ffe3012a01a3dab98f182dade7dfbe2fa781e2e
Amelieli <168002580+Amelieli@users.noreply.github.com>
2024-06-08 23:27:30 -0500

    HW exercise_5_Amelie.py

--- python_basics/class02/exercise_5.py
+++ python_basics/class02/exercise_5.py
@@ -5,10 +5,12 @@ Edit this file to complete Exercise 5
 # 1. rite a Python program to find those numbers which are divisible by 7 and multiple of 5, between 1500 and 2700 (both included).
 
 # code up your solution here
+list_mycode = []
+for i in range(1500, 2701):
+    if i%35 == 0:
+        list_mycode.append(i)
 
-
-
-
+print(list_mycode)
 
 # 2. Write a Python program to count the number of even and odd numbers from a series of numbers.
 
@@ -18,7 +20,17 @@ Edit this file to complete Exercise 5
 # >>> Number of odd numbers : 5
 
 # code up your solution here
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+even = []
+odd = []
+for i in numbers:
+    if i%2 == 0:
+        even.append(i)
+    elif i%2 !=0:
+        odd.append(i)
 
+print('Number of even numbers:', len(even))
+print('Number of odd numbers:', len(odd))
 
 
 
@@ -35,9 +47,15 @@ Edit this file to complete Exercise 5
 
 # code up your solution here
 
-
-
-
+for i in range(51):
+    if i%3 == 0 and i%5 == 0:
+        print('FizzBuzz')
+    elif i%5 == 0:
+        print('Buzz')
+    elif i % 3 ==0:
+        print('Fizz')
+    else:
+        print(i)
 
 # 4. Given a list iterate it and display numbers which are divisible by 5 and if you find number greater than 150 stop the loop iteration
 
@@ -49,15 +67,25 @@ Edit this file to complete Exercise 5
 # >>> 150
 
 # code up your solution here
-
-
-
+list1 = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
+for i in list1:
+    if i % 5 ==0 and i <= 150:
+        print(i)
 
 # 5. Pick one of the questions above and use range() for a different solution
 
 # code up your solution here
 
+even = []
+odd = []
+for i in range(1,10):
+    if i%2 == 0:
+        even.append(i)
+    elif i%2 !=0:
+        odd.append(i)
 
+print('Number of even numbers:', len(even))
+print('Number of odd numbers:', len(odd))
 
 
 # 6. Pick one of the question above and use comprehension for a different solution
@@ -70,4 +98,14 @@ Edit this file to complete Exercise 5
 # 7. Pcik one of the questions above and use while loop for a different solution
 
 # code up your solution here
+
+list1 = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
+i = 0
+while i <= len(list1):
+    number = list1[i]
+    if number > 150:
+        break
+    if number % 5 ==0:
+        print(number)
+    i += 1
 
